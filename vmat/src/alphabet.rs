896fb89e1ae4b516c9ada4a1328ccd0772018873
Paulo Fonseca <paguso@cin.ufpe.br>
2019-06-18 19:48:20 -0300

    one sequence lexicographic minimisers

---
+++ vmat/src/alphabet.rs
@@ -0,0 +1,161 @@
+use std::collections::HashMap;
+use std::hash::Hash;
+use std::ops::Index;
+
+pub trait Alphabet<T> 
+    where T: Eq
+{
+    fn len(&self) -> usize;
+    fn chr(&self, ord: usize) -> Option<&T>;
+    fn ord(&self, chr: &T) -> Option<usize>;
+}
+
+pub struct HashAlphabet<T> 
+    where T: Hash + Eq
+{
+    chr_vec: Vec<T>,
+    ord_map: HashMap<T, usize>
+}
+
+impl<T> HashAlphabet<T> 
+    where T: Copy + Hash + Eq 
+{
+    pub fn new(chr_vec: Vec<T>) -> HashAlphabet<T> {
+        let mut ord_map = HashMap::new();
+        let mut ord = 0;
+        for c in &chr_vec {
+            ord_map.insert(*c, ord);
+            ord += 1;
+        }
+        HashAlphabet{chr_vec, ord_map}
+    }
+}
+
+impl<T> Alphabet<T> for HashAlphabet<T> 
+    where T: Copy + Hash + Eq 
+{
+    fn len(&self) -> usize {
+        self.chr_vec.len()
+    }
+
+    fn chr(&self, ord: usize) -> Option<&T> {
+        self.chr_vec.get(ord)
+    }
+
+    fn ord(&self, chr: &T) -> Option<usize> {
+        match self.ord_map.get(chr) {
+            Some(val) => Some(*val),
+            None => None
+        }
+    }
+
+}
+
+impl<T> Index<usize> for HashAlphabet<T> 
+    where T: Copy + Hash + Eq
+{
+    type Output = T;
+    fn index(&self, idx: usize) -> &Self::Output {
+        &self.chr_vec[idx]
+    }
+}
+
+pub struct DNAAlphabet {
+    letters : [char; 4]
+}
+
+impl DNAAlphabet {
+    pub fn new() -> DNAAlphabet {
+        DNAAlphabet{letters: ['a','c','g','t']}
+    }
+}
+
+impl Alphabet<char> for DNAAlphabet {
+    fn len(&self) -> usize {
+        4
+    }
+
+    fn chr(&self, ord: usize) -> Option<&char> {
+        if ord<4 {
+            Some(&self.letters[ord])
+        }
+        else {
+            None
+        }
+    }
+
+    fn ord(&self, chr: &char) -> Option<usize> {
+        match chr {
+            'a' => Some(0),
+            'c' => Some(1),
+            'g' => Some(2),
+            't' => Some(3),
+            _ => None
+        }
+    }
+}
+
+impl Index<usize> for DNAAlphabet 
+{
+    type Output = char;
+    fn index(&self, idx: usize) -> &Self::Output {
+        &self.letters[idx]
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test] 
+    fn test_hashab_index() {
+        let chars = vec!['a','c','g','t'];
+        let ab = HashAlphabet::new(chars);
+        assert_eq!(ab[0], 'a');
+        assert_eq!(ab[1], 'c');
+    }
+    
+    #[test] 
+    fn test_hashab_chr() {
+        let chars = vec!['a','c','g','t'];
+        let ab = HashAlphabet::new(chars);
+        assert_eq!(*ab.chr(0).unwrap(), 'a');
+        assert_eq!(*ab.chr(1).unwrap(), 'c');
+        assert_eq!(ab.chr(6), None);
+    }
+    
+    #[test] 
+    fn test_hashab_ord() {
+        let chars = vec!['a','c','g','t'];
+        let ab = HashAlphabet::new(chars);
+        assert_eq!(ab.ord(&'a',).unwrap(), 0);
+        assert_eq!(ab.ord(&'g',).unwrap(), 2);
+        assert_eq!(ab.ord(&'_'), None );
+    }
+
+    #[test] 
+    fn test_dna_ab_index() {
+        let ab = DNAAlphabet::new();
+        assert_eq!(ab[0], 'a');
+        assert_eq!(ab[1], 'c');
+    }
+    
+    #[test] 
+    fn test_dna_ab_chr() {
+        let ab = DNAAlphabet::new();
+        assert_eq!(*ab.chr(0).unwrap(), 'a');
+        assert_eq!(*ab.chr(1).unwrap(), 'c');
+        assert_eq!(ab.chr(6), None);
+    }
+    
+    #[test] 
+    fn test_dna_ab_ord() {
+        let ab = DNAAlphabet::new();
+        assert_eq!(ab.ord(&'a',).unwrap(), 0);
+        assert_eq!(ab.ord(&'g',).unwrap(), 2);
+        assert_eq!(ab.ord(&'_'), None );
+    }
+
+
+
+}
--- vmat/src/lib.rs
+++ vmat/src/lib.rs
@@ -1,83 +1,129 @@
-use std::ops::Index;
-use std::collections::HashMap;
-use std::hash::Hash;
+use std::cmp;
+
+mod alphabet;
+use alphabet::Alphabet;
 
 
-trait Alphabet<T> 
-    where T: Eq
-{
-    fn len(&self) -> usize;
-    fn chr(&self, ord: usize) -> Option<&T>;
-    fn ord(&self, chr: &T) -> Option<usize>;
+
+trait StrRank {
+    fn rank(&self, s: &str) -> usize;
 }
 
-struct HashAlphabet<'a, T> 
-    where T: Hash + Eq  
-{
-    chr_vec: Vec<T>,
-    ord_map: HashMap<&'a T, usize>
+struct KmerLexRank<'a> {
+    ab: &'a alphabet::Alphabet<char>,
+    k: usize,
 }
 
-impl<'a, T> HashAlphabet<'a, T> 
-    where T: Hash + Eq 
-{
-    fn new(chr_vec: Vec<T>) -> HashAlphabet<'a, T> {
-        //let mut ord_map = HashMap::new();
-        let mut ret = HashAlphabet{chr_vec, ord_map:HashMap::new()};
-        let mut ord = 0;
-        for c in ret.chr_vec.iter() {
-            ret.ord_map.insert(&c, ord);
-            ord += 1;
+impl<'a> KmerLexRank<'a> {
+    fn new(k:usize, ab: &'a alphabet::Alphabet<char>) -> KmerLexRank {
+        KmerLexRank{k, ab}
+    }
+}
+
+impl<'a> StrRank for KmerLexRank<'a> {
+    fn rank(&self, s: &str) -> usize {
+       let mut rnk:usize = 0;
+       let mut schars = s.chars();
+       for _ in 0..self.k {
+            let c = schars.next().expect(
+                        &format!("String length >= {} required.", self.k)
+                    );
+            rnk = self.ab.len() * rnk + self.ab.ord(&c).expect(
+                &format!("Alphabet does not contain char '{}'.", c)
+            );
+       }
+       rnk 
+    }
+}
+
+///
+/// Return the (`w`, `k`)-minimisers' positions in `s`
+/// 
+fn find_minimisers(s: &str, w: usize, k:usize, rank: &StrRank) -> Option<Vec<usize>> {
+    let n = s.len();
+    if n < k || w == 0 {
+        // no kmer
+        return None; 
+    }
+    
+    // circular buffer to store current window kmer ranks
+    let mut buf:Vec<usize> = vec![];
+    let mut buf_start = 0;
+    // minimiser occurrences
+    let mut occ:Vec<usize> = vec![];
+    // last minimiser rank
+    let mut last_min_rank = std::usize::MAX;
+    // last minimiser rightmost position within window
+    let mut last_min_pos = 0;
+    // current kmer rank
+    let mut pos_rank;
+    // process initial window
+    println!("Processing initial window");
+    for j in 0..=cmp::min(w-1, n-k) {
+        pos_rank = rank.rank(&s[j..j+k]);
+        buf.push(pos_rank);
+        if pos_rank <= last_min_rank { // (!) <=
+            last_min_rank = pos_rank;
+            last_min_pos = j;
+        }      
+    }
+    for j in 0..=cmp::min(w-1, n-k) {
+        if buf[j] == last_min_rank {
+            occ.push(j);
         }
-        ret
     }
-}
-
-impl<'a, T> Alphabet<T> for HashAlphabet<'a, T> 
-    where T: Hash + Eq 
-{
-    fn len(&self) -> usize {
-        self.chr_vec.len()
-    }
-
-    fn chr(&self, ord: usize) -> Option<&T> {
-        self.chr_vec.get(ord)
-    }
-
-    fn ord(&self, chr: &T) -> Option<usize> {
-        match self.ord_map.get(chr) {
-            Some(val) => Some(*val),
-            None => None
+    // process subsequent windows, if any
+    if n > (w+k-1) {
+        assert_eq!(buf.len(), w);
+        // current window starts at position i
+        for i in 1..=n-(w+k-1) {
+            println!("Processing window at position {}",i);
+            // current window differs from previous by last kmer only
+            // process last kmer of the window
+            pos_rank = rank.rank(&s[i+w-1..i+w-1+k]);
+            buf[buf_start] = pos_rank;
+            buf_start = (buf_start + 1) % w;
+            if pos_rank == last_min_rank {
+                // last kmer is a new occurrence of last minimiser
+                // previous occurrences already accounted for
+                last_min_pos = w-1;
+                occ.push(i+w-1);
+            }
+            if pos_rank < last_min_rank {
+                // last kmer of current window is a new minimiser
+                last_min_rank = pos_rank;
+                last_min_pos = w-1;
+                for j in 0..w {
+                    if buf[(buf_start+j)%w] == last_min_rank {
+                        occ.push(i+j);
+                    }
+                }
+            }
+            else if last_min_pos == 0 {
+                // last kmer is not the minimiser 
+                // but last minimiser in no longer in the window
+                // must search for new window minimiser
+                last_min_rank = buf[buf_start];
+                last_min_pos = 0;
+                for j in 1..w {
+                    if buf[(buf_start+j)%w] <= last_min_rank  { // (!) <=
+                        last_min_rank = buf[(buf_start+j)%w];
+                        last_min_pos = j;
+                    }  
+                }
+                for j in 0..w {
+                    if buf[(buf_start+j)%w] == last_min_rank  {
+                        occ.push(i+j);
+                    }  
+                }
+            }
+            else {
+                // minimiser unchanged but window moved right
+                last_min_pos -= 1 ;
+            }
         }
     }
-
-}
-
-
-impl<'a, T> Index<usize> for HashAlphabet<'a, T> 
-    where T: Hash + Eq
-{
-    type Output = T;
-    fn index(&self, idx: usize) -> &Self::Output {
-        &self.chr_vec[idx]
-    }
-}
-
-
-trait KmerHasher {
-    fn hash(kmer: &str) -> usize;
-}
-
-struct LexKmerHasher {
-    alphabet: String
-}
-
-
-
-
-fn find_minimisers(src: &str, w: usize, k:usize) -> Vec<usize> {
-    let mut ret = vec![];
-    ret
+    Some(occ)
 }
 
 
@@ -85,22 +131,41 @@ fn find_minimisers(src: &str, w: usize, k:usize) -> Vec<usize> {
 mod tests {
     use super::*;
 
-    #[test] 
-    fn test_ab_index() {
-        let chars = vec!['a','c','g','t'];
-        let rank = HashRank::new(&chars);
-        let ab = Alphabet::new(chars, rank);
-        assert_eq!(ab[0], 'a');
-        assert_eq!(ab[1], 'c');
-    }
-    
-    #[test] 
-    fn test_ab_rank() {
-        let chars = vec!['a','c','g','t'];
-        let rank = HashRank::new(&chars);
-        let ab = Alphabet::new(chars, rank);
-        assert_eq!(ab.rank('a',).unwrap(), 0);
-        assert_eq!(ab.rank('g',).unwrap(), 2);
-        assert_eq!(ab.rank('_'), None );
+    #[test]
+    fn test_find_minimisers() {
+        let dna_ab = alphabet::DNAAlphabet::new();
+        let w = 4;
+        let k = 5;
+        let ranker = KmerLexRank::new(k, &dna_ab);
+        let s = String::from("acgtacgtacgtacgtacgtacgtacgtacgtacgtacgt");
+        match find_minimisers(&s, w, k, &ranker) {
+            Some(occ) => {
+                for j in &occ {
+                    assert_eq!(&s[..k], &s[*j..*j+k]);
+                    println!("Found minimiser {} at position {}", &s[*j..*j+k], *j);
+                }
+            },
+            None => assert!(false, "No minimisers found. Should have found some."),
+        }
+        let w = 1;
+        let k = 500;
+        let ranker = KmerLexRank::new(k, &dna_ab);
+        assert_eq!(find_minimisers(&s, w, k, &ranker), None); 
+        let w = 0;
+        let k = 5;
+        let ranker = KmerLexRank::new(k, &dna_ab);
+        assert_eq!(find_minimisers(&s, w, k, &ranker), None); 
+        let w = 5;
+        let k = 0;
+        let ranker = KmerLexRank::new(k, &dna_ab);
+        match find_minimisers(&s, w, k, &ranker) {
+            Some(occ) => {
+                for j in &occ {
+                    assert_eq!(&s[..k], &s[*j..*j+k]);
+                    println!("Found minimiser {} at position {}", &s[*j..*j+k], *j);
+                }
+            },
+            None => println!("No minimisers found"),
+        }
     }
 }
