0ffe898cd492b6a4c6c2b8a63b42302505d385ef
Paulo Fonseca <paguso@cin.ufpe.br>
2019-08-19 23:25:38 -0300

    wip

--- vmat/src/lib.rs
+++ vmat/src/lib.rs
@@ -1,3 +1,4 @@
 mod alphabet;
 mod minimiser;
 mod xstring;
+mod xstream;
---
+++ vmat/src/xstream.rs
@@ -0,0 +1,88 @@
+use std::io::BufRead;
+use std::io::Read;
+use std::io::BufReader;
+use std::io::Seek;
+use std::io::SeekFrom;
+use std::fs::File;
+
+
+pub struct XStream<R> 
+where R: Read + Seek
+{
+    reader: R,
+    buf: Vec<u8>,
+    bfrom: usize,
+    bto: usize,
+    bstart: usize,
+    fpos: u64,
+}
+
+impl<R> XStream<R> 
+where R: Read + Seek
+{
+    fn new(reader: R) -> Self {
+        XStream {
+            reader: reader,
+            buf: Vec::new(),
+            bfrom: 0,
+            bto: 0,
+            bstart: 0,
+            fpos: 0
+        }
+    }
+
+    fn read_to_buf(&mut self, offset:u64, nbytes: usize) -> Result<usize, std::io::Error> {
+        self.fpos = self.reader.seek(SeekFrom::Start(offset))?;
+        if nbytes > self.buf.len() {
+            self.buf.resize(nbytes, 0);
+        }
+        let n = self.reader.read(&mut self.buf[..nbytes])?;
+        self.bfrom = self.fpos as usize;
+        self.bto = self.bfrom + n;
+        Ok(n)
+    }
+
+}
+
+impl<I> Index<I> for XStream
+where
+    I: SliceIndex<[u8]>,
+{
+    type Output = <I as SliceIndex<[u8]>>::Output;
+
+    fn index(&self, index: I) -> &Self::Output {
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    use std::fs::File;
+    use std::io::BufReader;
+    use std::io::BufWriter;
+    use std::io::Read;
+    use std::io::Write;
+
+    fn test_setup() -> std::io::Result<()> {
+        let mut v:Vec<u8> = vec![0];
+        for i in 0..=255 {
+            v.push(i);
+        }
+        let mut w = BufWriter::new(File::create("test.txt")?);
+        for i in 0..=255 {
+            v[i] = i as u8;
+            w.write_all(&v)?;
+        }
+        w.flush()?;
+        Ok(()) 
+    }
+
+    #[test]
+    fn test_read() -> std::io::Result<()> {
+        let s = XStream::new(BufReader::new(File::open("test.txt")?));
+
+        Ok(())
+    }
+}
