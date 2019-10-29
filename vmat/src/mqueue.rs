ef5a23d0be6715f373ce46b9320179c3c68bc34e
Paulo Fonseca <paguso@cin.ufpe.br>
2019-10-29 17:03:18 -0300

    wip

--- vmat/src/mqueue.rs
+++ vmat/src/mqueue.rs
@@ -1,14 +1,44 @@
 use crate::mstack::MStack;
+use std::collections::VecDeque;
 use std::cmp::Ordering;
+
+
+pub struct MQueueXtrIter<'a, T> 
+where T: Ord 
+{
+    src: &'a MQueue<T>,
+    index: usize,
+}
+
+
+impl<'a, T> Iterator for  MQueueXtrIter<'a, T>  
+where T: Ord
+{
+    type Item: &'a T;
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.src.len()==0 {
+            return None
+        }
+        else if self.index == 0 {
+            self.index += 1;
+            return self.src.queue.get(self.src.minmax.get(0).unwrap() - self.src.popped); 
+        }
+        else if self.index >= self.src.minmax.len() || self.src.get(self.
+
+    }
+
+}
+
 
 #[derive(Clone)]
 pub struct MQueue<T>
 where
     T: Ord,
 {
-    front: MStack<T>,
-    rear: MStack<T>,
+    queue: VecDeque<T>,
+    minmax: VecDeque<usize>,
     crit: Ordering,
+    popped: usize,
 }
 
 impl<T> MQueue<T>
@@ -17,22 +47,24 @@ where
 {
     pub fn new_min() -> Self {
         MQueue {
-            front: MStack::new_min(),
-            rear: MStack::new_min(),
+            queue: VecDeque::new(),
+            minmax: VecDeque::new(),
             crit: Ordering::Less,
+            popped: 0,
         }
     }
 
     pub fn new_max() -> Self {
         MQueue {
-            front: MStack::new_max(),
-            rear: MStack::new_max(),
+            queue: VecDeque::new(),
+            minmax: VecDeque::new(),
             crit: Ordering::Greater,
+            popped: 0,
         }
     }
 
     pub fn len(&self) -> usize {
-        self.front.len() + self.rear.len()
+        self.queue.len()
     }
 
     pub fn is_empty(&self) -> bool {
@@ -40,31 +72,31 @@ where
     }
 
     pub fn push(&mut self, item: T) {
-        self.rear.push(item)
+        let l = self.len();
+        while self.minmax.len() > 0 && item.cmp(&self.queue[self.minmax.back().unwrap()-self.popped]) == self.crit {
+            self.minmax.pop_back();
+        }
+        self.queue.push_back(item);
+        self.minmax.push_back(self.popped + l);
     }
 
     pub fn pop(&mut self) -> Option<T> {
-        if self.front.is_empty() {
-            while !self.rear.is_empty() {
-                self.front.push(self.rear.pop().unwrap());
-            }
+        if self.queue.is_empty() {
+            return None;
         }
-        self.front.pop()
+        if *self.minmax.front().unwrap() == self.popped {
+            self.minmax.pop_front();
+        }
+        self.popped += 1;
+        self.queue.pop_front()
     }
 
     pub fn xtr(&self) -> Option<&T> {
-        if self.front.is_empty() {
-            self.rear.xtr()
-        } else if self.rear.is_empty() {
-            self.front.xtr()
-        } else {
-            let f = self.front.xtr();
-            let r = self.rear.xtr();
-            if f.unwrap().cmp(r.unwrap()) == self.crit {
-                f
-            } else {
-                r
-            }
+        if self.is_empty() {
+            return None;
+        }
+        else {
+            return self.queue.get(self.minmax.front().unwrap() - self.popped);   
         }
     }
 }
@@ -77,16 +109,22 @@ mod tests {
     fn test_push_pop_min() {
         let mut queue: MQueue<u32> = MQueue::new_min();
         queue.push(30);
+        println!("{0:?} {1:?}", queue.queue, queue.minmax);
         assert_eq!(*queue.xtr().unwrap(), 30 as u32);
         queue.push(20);
+        println!("{0:?} {1:?}", queue.queue, queue.minmax);
         assert_eq!(*queue.xtr().unwrap(), 20 as u32);
         queue.push(40);
+        println!("{0:?} {1:?}", queue.queue, queue.minmax);
         assert_eq!(*queue.xtr().unwrap(), 20 as u32);
         queue.pop();
+        println!("{0:?} {1:?}", queue.queue, queue.minmax);
         assert_eq!(*queue.xtr().unwrap(), 20 as u32);
         queue.pop();
+        println!("{0:?} {1:?}", queue.queue, queue.minmax);
         assert_eq!(*queue.xtr().unwrap(), 40 as u32);
         queue.pop();
+        println!("{0:?} {1:?}", queue.queue, queue.minmax);
         assert!(queue.pop().is_none());
     }
 
